5247e536b092238843ae484861459ff0aa16d353
JiteshVarade <jiteshvarade1@gmail.com>
2024-07-13 01:03:14 +0530

    Added basic server code

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const {PORT} = require('./constant');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+const ConnectDB = require('./src/db/connection');
+
+if(require.main === module){
+
+    ConnectDB();
+
+    const app = express();
+    app.use(express.json());
+
+    const server = createServer(app);
+    const io = new Server(server, {
+        cors : { origin : true }
+    });
+
+    io.on("connection", (socket) => {
+        console.og("User connected : ",socket.id);
+    });
+    
+    app.use(cors({origin : true}));
+    
+    app.listen(PORT, () => {
+        console.log('Server started successfully');
+        console.log(`http://localhost:${PORT}`);
+    });
+    
+    app.get('/', (req,res) => {
+        res.send("Welcome to EcoSathi Server");
+    });
+}
---
+++ server/src/models/ragpicker.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ragPickerSchema = new mongoose.Schema({
+
+},
+{
+    timestamps : true
+}); 
+
+const RagPicker = mongoose.model("RagPicker",ragPickerSchema);
+
+module.exports = User;

f991a0c94efd13d5960f98c280594206a1b0c269
Nitish <nitishrsm@gmail.com>
2024-07-12 23:39:34 +0530

    Added react router and react icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   function loadRazorPay() {
     const script = document.createElement("script");
@@ -17,7 +12,9 @@ function App() {
 
   return (
     <>
-      <div className="visme_d" data-title="Contact Us Contact Form" data-url="rx7oqp3o-contact-us-contact-form?fullPage=true" data-domain="forms" data-full-page="true" data-min-height="100vh" data-form-id="82362"></div>
+      {/* <div className="visme_d" data-title="Contact Us Contact Form" data-url="rx7oqp3o-contact-us-contact-form?fullPage=true" data-domain="forms" data-full-page="true" data-min-height="100vh" data-form-id="82362"></div> */}
+
+      <h1>header</h1>
     </>
   )
 }

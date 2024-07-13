146dfa4cbd89d1223384b51ca608febb6eeb158b
JiteshVarade <jiteshvarade1@gmail.com>
2024-07-13 19:21:51 +0530

    Added auth.js

--- server/src/models/organisation.js
+++ server/src/models/organisation.js
@@ -1,7 +1,41 @@
 const mongoose = require('mongoose');
 
 const organisationSchema = new mongoose.Schema({
-
+    userInfo : {
+        type : Object,
+        required : true,
+    },
+    earnings : {
+        type : [{
+            amount : Number,
+            time : Date,
+        }],
+        required : false,
+        default : [],
+    },
+    organisationData : {
+        type : Object,
+        required : true,
+    },
+    notifications : {
+        type : [{
+            from : String,
+            address : String,
+            status : Boolean,
+            time : Date,
+        }],
+        required : false,
+        default : [],
+    },
+    ragPickers : {
+        type : [{
+            name : String,
+            phoneNo : String,
+            address : String,
+        }],
+        required : false,
+        default : [],
+    }
 },
 {
     timestamps : true
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,7 +1,40 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-
+    userInfo : {
+        type : Object,
+        required : true,
+    },
+    earnings : {
+        type : [{
+            amount : Number,
+            time : Date,
+        }],
+        required : false,
+        default : [],
+    },
+    currentRagPicker : {
+        type : [{
+            ragPicker : String,
+            address : String,
+            amount : Number,
+            status : Boolean,
+            time : Date,
+        }],
+        required : false,
+        default : [],
+    },
+    history : {
+        type : [{
+            ragPicker : String,
+            address : String,
+            amount : Number,
+            status : Boolean,
+            time : Date,
+        }],
+        required : false,
+        default : [],
+    }
 },
 {
     timestamps : true

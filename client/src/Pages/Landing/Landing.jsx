09c4bd491445ac23346301e1f294d28086873b52
JiteshVarade <jiteshvarade1@gmail.com>
2024-07-13 09:39:30 +0530

    resolved merge conflict

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-import NavBar from '../../components/NavBar/NavBar'
-import Updates from '../../components/LatestUpdate/Updates'
-import Testimonial from '../../components/Testimonials/Testimonial'
-
-export default function Landing() {
-  return (
-    <div className='relative p-4'>
-    <NavBar />
-    <Updates/>
-    <Testimonial/>
-=======
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import LandingHero from "../../components/Landing Hero/LandingHero";
 import Updates from "../../components/LatestUpdate/Updates";
+import Testimonial from '../../components/Testimonials/Testimonial'
 
 export default function Landing() {
   return (
@@ -22,7 +10,7 @@ export default function Landing() {
       <NavBar />
       <LandingHero />
       <Updates />
->>>>>>> 1566aead4751045fe869abd815f57d26e2052740
+      <Testimonial/>
     </div>
   );
 }
